673a3ab617def948ff3e64f42be43265695d1f72
JihyeugJang <75356273+JihyeugJang@users.noreply.github.com>
2025-05-22 11:21:58 +0200

    Add files via upload

---
+++ CV/auto/CV.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "CV"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "letterpaper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("mathpazo" "sc" "osf")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "hyperref"
+    "geometry"
+    "marvosym"
+    "fontenc"
+    "mathpazo"
+    "sectsty")
+   (TeX-add-symbols
+    "name"
+    "footerlink"))
+ :latex)
+
